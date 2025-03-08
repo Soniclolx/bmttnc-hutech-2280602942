29b21ff2bb83f365aa3ac022d5b6fb88cc8935da
soniclolx <no123lv1@gmail.com>
2025-03-08 16:24:05 +0700

    [add] lab-02 transposition

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,3 +1,18 @@
+from cipher.vigenere import VigenereCipher
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    text = request.json['plain_text']
+    key = request.json['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    text = request.json['text']
+    key = request.json['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
@@ -12,7 +27,7 @@ def encrypt():
     return jsonify({'encrypted_text': encrypted_text})
 @app.route('/decrypt', methods=['POST'])
 def caesar_decrypt():
-    text = request.json['text']
+    text = request.json['cipher_text']
     key = request.json['key']
     decrypted_text = caesar_cipher.decrypt_text(text, key)
     return jsonify({'decrypted_text': decrypted_text})
