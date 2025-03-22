dcb2bf0f443ace546c561a49671e264270a92671
Soniclolx <no123lv1@gmail.com>
2025-03-22 15:12:06 +0700

    [add] lab-04 aes_rsa_socket

---
+++ lab-04/aes_rsa_socket/client.py
@@ -0,0 +1,52 @@
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad,unpad
+import socket
+import threading
+import hashlib
+
+#intialize client socket
+client_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+#generate RSA key pair
+client_key = RSA.generate(2048)
+#receive server's public key
+server_public_key = RSA.import_key(client_socket.recv(2048))
+#send client's public key
+client_socket.send(client_key.publickey().export_key(format='PEM'))
+#receive encrypted AES key
+encrypted_aes_key = client_socket.recv(2048)
+#decrypt AES key
+cipher_rsa = PKCS1_OAEP.new(client_key)
+aes_key = cipher_rsa.decrypt(encrypted_aes_key)
+#function to encrypt message
+def encrypt_message(key, message):
+    cipher = AES.new(key, AES.MODE_CBC)
+    cipher_text = cipher.encrypt(pad(message.encode(), AES.block_size))
+    return cipher.iv + cipher_text
+#function to decrypt message
+def decrypt_message(key, message):
+    iv = message[:AES.block_size]
+    cipher_text = message[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return decrypt_message.decode()
+#function to receive messages
+def receive_message():
+    while True:
+        encrypt_message = client_socket.recv(1024)
+        decrypt_message = decrypt_message(aes_key, encrypt_message)
+        print(f"Received : {decrypt_message}")
+#start a thread to receive thread
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+#send messages
+while True:
+    message = input()
+    encrypted = encrypt_message(aes_key, message)
+    client_socket.send(encrypted)
+    if message == "exit":
+        break
+#close the connection
+client_socket.close()
