ca14d4cf97abad1f29a4de383836a6ab91727f1f
NguyenChiThanh-2280602942 <no123lv1@gmail.com>
2025-04-05 15:37:29 +0700

    Delete lab-01 directory

--- lab-01/ex02_03.py
+++
@@ -1,6 +0,0 @@
-#Nhập số
-so = int(input("Nhập 1 số nguyên"))
-#Kiểm tra chẵn 
-if so % 2==0:
-    print(so, "Là số chẵn")
-else: print(so,"Không phải số chẵn")
--- lab-01/ex02_10.py
+++
@@ -1,5 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-#sử dụng hàm và in
-input_string = input("Nhập chuỗi: ")
-print("Chuỗi đảo ngược là:",dao_nguoc_chuoi(input_string))
